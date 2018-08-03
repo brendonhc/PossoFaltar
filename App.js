e10564b1e1d7e1c2d281cb3cbd2e9ae8ecd87015
Brendon Hudson <brendonhc@outlook.com>
2018-08-03 20:10:20 -0300

    hello

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+    render() {
+        return ( // JSX
+            <View style={styles.mainview}>
+                <Text>Hello World!</Text>
+                <Text style={styles.biggreen}>Hello dear Father, Augusto!!!</Text>
+                <Text style={styles.bigblue}>I'm learning React Native</Text>
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+// CSS
+const styles = StyleSheet.create ({
+    mainview: {
+        padding: 10% 0,
+    },
+
+    biggreen: {
+        color: 'green',
+        fontWeight: 'bold',
+        fontSize: 50,
+    },
+
+    bigblue: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
 });
